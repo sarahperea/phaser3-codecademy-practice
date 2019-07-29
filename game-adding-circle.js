863a45b0b8f2cf30878571cc7d226e45a7060ad9
Sarah D. Perea <sdperea1@up.edu.ph>
2019-07-29 20:52:01 +0800

    Fix indentation

--- game-adding-circle.js
+++ game-adding-circle.js
@@ -3,18 +3,16 @@ function create() {
   let circle2 = this.add.circle(95, 300, 80, 0xFF0000);
   let circle3 = this.add.circle(200, 200, 100, 0x9F00D0);
   let circle4 = this.add.circle(300, 400, 10, 0x00E030);
-
-  // Add in a circle here!
   let circle5 = this.add.circle(200,200,50,0x00E035);
 }
 
 const config = {
-	type: Phaser.AUTO,
-	width: 450,
-	height: 600,
-	scene: {
+  type: Phaser.AUTO,
+  width: 450,
+  height: 600,
+  scene: {
     create
-	}
+  }
 }
 
 const game = new Phaser.Game(config)
