9bfd8b6ad5817bcccdbc981fb470c4d2c800b304
Sarah D. Perea <sdperea1@up.edu.ph>
2019-08-01 20:30:48 +0800

    Detect collisions, setCollideWorldBounds()

--- game-physics.js
+++ game-physics.js
@@ -11,9 +11,13 @@ const gameState = {};
 function create() {
   gameState.player = this.physics.add.sprite(225, 440, 'codey').setScale(.5);
   
-  // Add your code below:
   const platforms = this.physics.add.staticGroup();
-  platforms.create(225,510,'platform');
+
+  platforms.create(225, 510, 'platform');
+
+  // Add your code below:
+  gameState.player.setCollideWorldBounds(true)
+	this.physics.add.collider(gameState.player, platforms);
 }
 
 function update() {
