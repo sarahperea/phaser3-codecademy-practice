d27efb167ff7cef094baa4754fd3f2b56955a75e
Sarah D. Perea <sdperea1@up.edu.ph>
2019-08-01 20:26:58 +0800

    Static Groups

--- game-physics.js
+++ game-physics.js
@@ -1,16 +1,19 @@
 function preload() {
-  this.load.image('bug1', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_1.png')
-  this.load.image('bug2', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_2.png')
-  this.load.image('bug3', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_3.png')
-  this.load.image('platform', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/platform.png')
-  this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/codey.png')
+  this.load.image('bug1', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_1.png');
+  this.load.image('bug2', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_2.png');
+  this.load.image('bug3', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_3.png');
+  this.load.image('platform', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/platform.png');
+  this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/codey.png');
 }
 
 const gameState = {};
 
 function create() {
-  // Add your code below: 
-  this.physics.add.sprite(150, 100, 'codey');
+  gameState.player = this.physics.add.sprite(225, 440, 'codey').setScale(.5);
+  
+  // Add your code below:
+  const platforms = this.physics.add.staticGroup();
+  platforms.create(225,510,'platform');
 }
 
 function update() {
@@ -22,10 +25,10 @@ const config = {
   height: 500,
   backgroundColor: "b9eaff",
   physics: {
-    default: 'arcade', //use Arcade physics plugin
+    default: 'arcade',
     arcade: {
-      gravity: { y: 300 },
-      debug: true
+      gravity: { y: 200 },
+      enableBody: true,
     }
   },
   scene: {
@@ -33,7 +36,7 @@ const config = {
     create,
     update
   }
-}
+};
 
-const game = new Phaser.Game(config)
+const game = new Phaser.Game(config);
 
