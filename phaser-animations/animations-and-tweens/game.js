bf03cf3eebeb152c96742b8b8e0577584e77d8cb
Sarah D. Perea <sdperea1@up.edu.ph>
2019-08-04 15:50:58 +0800

    Initial commit

---
+++ phaser-animations/animations-and-tweens/game.js
@@ -0,0 +1,59 @@
+const gameState = {};
+
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'GameScene' });
+  }
+
+  preload() {
+    this.load.image('cave', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Cave+Crisis/cave_background.png');
+    this.load.image('platform', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Cave+Crisis/platform.png');
+
+    // Load in the sprite sheet below:
+		
+    
+  }
+
+
+  create() {
+    // gameState.active is true if the game is playble (not game over)
+    gameState.active = true;
+
+    // Creates the background image
+    this.add.image(0, 0, 'cave').setOrigin(0, 0);
+
+    // Creates platforms group
+    const platforms = this.physics.add.staticGroup();
+    // An array of platform positions that is iterated over to create the platforms 
+    const platPositions = [
+      { x: 50, y: 575 }, { x: 250, y: 575 }, { x: 450, y: 575 }, { x: 400, y: 380 }, { x: 100, y: 200 },
+    ];
+    platPositions.forEach(plat => {
+      platforms.create(plat.x, plat.y, 'platform');
+    });    
+  }
+
+  update() {
+    
+  }
+}
+
+
+const config = {
+  type: Phaser.AUTO,
+  width: 500,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 1500 },
+      enableBody: true,
+    }
+  },
+  scene: [GameScene]
+};
+
+const game = new Phaser.Game(config);
+
+
+
