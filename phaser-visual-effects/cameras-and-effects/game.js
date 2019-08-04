b8ae30277ce38897b1a09d0b58476b0ccf3c0ed1
Sarah D. Perea <sdperea1@up.edu.ph>
2019-08-04 18:58:28 +0800

    Initial commit

---
+++ phaser-visual-effects/cameras-and-effects/game.js
@@ -0,0 +1,112 @@
+class Level extends Phaser.Scene {
+  constructor() {
+    super()
+  }
+
+  preload() {
+    this.load.image('platform', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Codey+Tundra/platform.png');
+    this.load.image('snowflake', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Codey+Tundra/snowflake.png');
+    this.load.spritesheet('campfire', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Codey+Tundra/campfire.png',
+      { frameWidth: 32, frameHeight: 32});
+    this.load.spritesheet('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Codey+Tundra/codey.png', { frameWidth: 72, frameHeight: 90})
+
+    this.load.image('bg1', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Codey+Tundra/mountain.png');
+    this.load.image('bg2', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Codey+Tundra/trees.png');
+    this.load.image('bg3', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Codey+Tundra/snowdunes.png');
+  }
+
+  create() {
+    gameState.active = true
+
+    gameState.bg3 = this.add.image(0, 0, 'bg3').setOrigin(0)
+
+    gameState.player = this.physics.add.sprite(125, 110, 'codey').setScale(.5);
+
+    gameState.platforms = this.physics.add.staticGroup();
+
+    this.createAnimations();
+
+    // set Cameras here
+
+
+    gameState.player.setCollideWorldBounds(true);
+
+    this.physics.add.collider(gameState.player, gameState.platforms);
+
+    gameState.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  createAnimations() {
+    this.anims.create({
+      key: 'run',
+      frames: this.anims.generateFrameNumbers('codey', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'idle',
+      frames: this.anims.generateFrameNumbers('codey', { start: 4, end: 5 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'jump',
+      frames: this.anims.generateFrameNumbers('codey', { start: 2, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+    })
+
+    this.anims.create({
+      key: 'fire',
+      frames: this.anims.generateFrameNumbers('campfire'),
+      frameRate: 10,
+      repeat: -1
+    })
+  }
+
+  update() {
+    if(gameState.active){
+      if (gameState.cursors.right.isDown) {
+        gameState.player.flipX = false;
+        gameState.player.setVelocityX(gameState.speed);
+        gameState.player.anims.play('run', true);
+      } else if (gameState.cursors.left.isDown) {
+        gameState.player.flipX = true;
+        gameState.player.setVelocityX(-gameState.speed);
+        gameState.player.anims.play('run', true);
+      } else {
+        gameState.player.setVelocityX(0);
+        gameState.player.anims.play('idle', true);
+      }
+    }
+  }
+}
+
+const gameState = {
+  speed: 240,
+  ups: 380,
+  width: 2000,
+  height: 600,
+};
+
+const config = {
+  type: Phaser.AUTO,
+  width: 500,
+  height: 600,
+  fps: {target: 60},
+  backgroundColor: "b9baff",
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 800 },
+      enableBody: true,
+
+    }
+  },
+  scene: [Level]
+};
+
+const game = new Phaser.Game(config);
+
