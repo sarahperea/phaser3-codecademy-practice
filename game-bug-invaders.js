541ebff51d1d3abeb0bf185b45ea324d6cc3b38f
Sarah D. Perea <sdperea1@up.edu.ph>
2019-08-04 13:07:52 +0800

    'Player won' state

--- game-bug-invaders.js
+++ game-bug-invaders.js
@@ -1,26 +1,17 @@
 function preload() {
-  this.load.image('bug1', 
-'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bug_1.png');
-  this.load.image('bug2', 
-'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bug_2.png');
-  this.load.image('bug3', 
-'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bug_3.png');
-  this.load.image('platform', 
-'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/platform.png');
-  this.load.image('codey', 
-'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/codey.png');
-  this.load.image('bugPellet', 
-'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bugPellet.png');
-  this.load.image('bugRepellent', 
-'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bugRepellent.png');
+  this.load.image('bug1', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bug_1.png');
+  this.load.image('bug2', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bug_2.png');
+  this.load.image('bug3', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bug_3.png');
+  this.load.image('platform', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/platform.png');
+  this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/codey.png');
+  this.load.image('bugPellet', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bugPellet.png');
+  this.load.image('bugRepellent', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Bug+Invaders/bugRepellent.png');
 }
 
 // Helper Methods below:
-// sortedEnemies() returns an array of enemy sprites sorted by their x 
-coordinate
+// sortedEnemies() returns an array of enemy sprites sorted by their x coordinate
 function sortedEnemies(){
-  const orderedByXCoord = gameState.enemies.getChildren().sort((a, b) => 
-a.x - b.x);
+  const orderedByXCoord = gameState.enemies.getChildren().sort((a, b) => a.x - b.x);
   return orderedByXCoord;
 }
 // numOfTotalEnemies() returns the number of total enemies 
@@ -32,12 +23,10 @@ function numOfTotalEnemies() {
 const gameState = {};
 
 function create() {
-	// When gameState.active is true, the game is being played and 
-not over. When gameState.active is false, then it's game over
+	// When gameState.active is true, the game is being played and not over. When gameState.active is false, then it's game over
 	gameState.active = true;
 
-	// When gameState.active is false, the game will listen for a 
-pointerup event and restart when the event happens
+	// When gameState.active is false, the game will listen for a pointerup event and restart when the event happens
 	this.input.on('pointerup', () => {
 		if (gameState.active === false) {
 			this.scene.restart();
@@ -46,17 +35,13 @@ pointerup event and restart when the event happens
 
 	// Creating static platforms
 	const platforms = this.physics.add.staticGroup();
-	platforms.create(225, 490, 'platform').setScale(1, 
-.3).refreshBody();
+	platforms.create(225, 490, 'platform').setScale(1, .3).refreshBody();
 
-	// Displays the initial number of bugs, this value is initially 
-hardcoded as 24 
-	gameState.scoreText = this.add.text(175, 482, 'Bugs Left: 24', { 
-fontSize: '15px', fill: '#000000' });
+	// Displays the initial number of bugs, this value is initially hardcoded as 24 
+	gameState.scoreText = this.add.text(175, 482, 'Bugs Left: 24', { fontSize: '15px', fill: '#000000' });
 
 	// Uses the physics plugin to create Codey
-	gameState.player = this.physics.add.sprite(225, 450, 
-'codey').setScale(.5);
+	gameState.player = this.physics.add.sprite(225, 450, 'codey').setScale(.5);
 
 	// Create Collider objects
 	gameState.player.setCollideWorldBounds(true);
@@ -69,16 +54,14 @@ fontSize: '15px', fill: '#000000' });
 	gameState.enemies = this.physics.add.group();	
   for (let yVal=1; yVal<4; yVal++) {
     for (let xVal=1; xVal<9; xVal++) {
-      gameState.enemies.create(50*xVal, 50*yVal, 
-'bug1').setScale(.6).setGravityY(-200);
+      gameState.enemies.create(50*xVal, 50*yVal, 'bug1').setScale(.6).setGravityY(-200);
     }
   }
   
   const pellets = this.physics.add.group();
   
   function genPellet() {
-    let randomBug = 
-Phaser.Utils.Array.GetRandom(gameState.enemies.getChildren());
+    let randomBug = Phaser.Utils.Array.GetRandom(gameState.enemies.getChildren());
     pellets.create(randomBug.x, randomBug.y, 'bugPellet');
   }
   
@@ -104,12 +87,12 @@ Phaser.Utils.Array.GetRandom(gameState.enemies.getChildren());
   
   gameState.bugRepellent = this.physics.add.group();
   
-  this.physics.add.collider(gameState.bugRepellent, gameState.enemies, 
-(bug, repellent) => {
+  this.physics.add.collider(gameState.bugRepellent, gameState.enemies, (bug, repellent) => {
     bug.destroy();
     repellent.destroy();
     gameState.scoreText.setText(`Bugs Left: ${numOfTotalEnemies()}`)
   })
+  
 }
 
 function update() {
@@ -124,16 +107,19 @@ function update() {
 		}
 
 		// Execute code if the spacebar key is pressed
-		if 
-(Phaser.Input.Keyboard.JustDown(gameState.cursors.space)) {
-			
-gameState.bugRepellent.create(gameState.player.x, gameState.player.y, 
-'bugRepellent').setVelocityY(-400);
+		if (Phaser.Input.Keyboard.JustDown(gameState.cursors.space)) {
+			gameState.bugRepellent.create(gameState.player.x, gameState.player.y, 'bugRepellent').setVelocityY(-500);
 		}
 
-		// Add logic for winning condition and enemy movements 
-below:
-    
+		// Add logic for winning condition and enemy movements below:
+    if (numOfTotalEnemies() === 0) {
+      gameState.active = false;
+      this.physics.pause();
+      this.add.text(180,180,'You won!', {
+        fontSize: '15px',
+        fill: '#000000'
+      });
+    }
   }
 }
 
