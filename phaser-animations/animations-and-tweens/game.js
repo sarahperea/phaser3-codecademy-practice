c48fcf9f57c1349edf6f3fc58ed5d65624f7c0d9
Sarah D. Perea <sdperea1@up.edu.ph>
2019-08-04 16:31:29 +0800

    Create animation

--- phaser-animations/animations-and-tweens/game.js
+++ phaser-animations/animations-and-tweens/game.js
@@ -8,33 +8,40 @@ class GameScene extends Phaser.Scene {
   preload() {
     this.load.image('cave', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Cave+Crisis/cave_background.png');
     this.load.image('platform', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Cave+Crisis/platform.png');
-
-    // Load in the sprite sheet below:
-    this.load.spritesheet('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Cave+Crisis/codey_sprite.png', { frameWidth: 72, frameHeight: 90 });		
-    
+    this.load.spritesheet('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Cave+Crisis/codey_sprite.png', { frameWidth: 72, frameHeight: 90 });
   }
 
 
   create() {
-    // gameState.active is true if the game is playble (not game over)
     gameState.active = true;
 
-    // Creates the background image
     this.add.image(0, 0, 'cave').setOrigin(0, 0);
 
-    // Creates platforms group
     const platforms = this.physics.add.staticGroup();
-    // An array of platform positions that is iterated over to create the platforms 
     const platPositions = [
       { x: 50, y: 575 }, { x: 250, y: 575 }, { x: 450, y: 575 }, { x: 400, y: 380 }, { x: 100, y: 200 },
     ];
     platPositions.forEach(plat => {
-      platforms.create(plat.x, plat.y, 'platform');
-    });    
+      platforms.create(plat.x, plat.y, 'platform')
+    });
+
+    // Add your code below:
+	  gameState.player = this.physics.add.sprite(255,500,'codey');
+    
+    this.anims.create({
+      key: 'run',
+      frames: this.anims.generateFrameNumbers('codey', { start: 0, end: 3 }),
+      frameRate: 5,
+      repeat: -1
+    });
+
+
+    // Collider object so Codey doesn't slip through the platforms
+    this.physics.add.collider(gameState.player, platforms);
   }
 
   update() {
-    
+
   }
 }
 
@@ -43,6 +50,7 @@ const config = {
   type: Phaser.AUTO,
   width: 500,
   height: 600,
+  backgroundColor: "b9eaff",
   physics: {
     default: 'arcade',
     arcade: {
