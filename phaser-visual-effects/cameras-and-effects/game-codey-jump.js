5e19c8eef16f096f732361b6e44c9c9b3052a554
Sarah D. Perea <sdperea1@up.edu.ph>
2019-08-05 21:38:05 +0800

    Initial commit for Codey Jump

---
+++ phaser-visual-effects/cameras-and-effects/game-codey-jump.js
@@ -0,0 +1,113 @@
+let game;
+let platforms;  // a group of platform objects the player will jump on
+let player; // the actual player controlled sprite
+let cursors;
+let platformCount = 0;
+let emitter;
+let particles;
+let gameOptions = {
+  width: 480,
+  height: 640,
+  gravity: 800
+}
+
+class JumpScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'JumpScene' })
+  }
+
+  preload(){
+    this.load.image('platform', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Codey+Jump/platform.png');
+    this.load.image('stripe', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Codey+Jump/stripe.png');
+    this.load.spritesheet("codey", "https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/Codey+Jump/codey.png", {
+      frameWidth: 72,
+      frameHeight: 90
+    });
+  }
+
+  create() {
+
+    this.physics.world.setBounds(0, 0, 480, 640);
+    platforms = this.physics.add.group({
+      allowGravity: false,
+      immovable: true,
+    });
+
+    for (let i = 0; i < 8; i++) {
+      let randomX = Math.floor(Math.random() * 400) + 24;
+      platforms.create(randomX, i * 80, 'platform').setScale(.5);
+    }
+
+    player = this.physics.add.sprite(100, 450, 'codey').setScale(.5);
+    player.setBounce(1);
+    player.setCollideWorldBounds(true);
+    player.body.checkCollision.up = false;
+    player.body.checkCollision.left = false;
+    player.body.checkCollision.right = false;
+
+    //In order for our platforms to interact with the player we add a collider  
+    this.physics.add.collider(player, platforms);
+
+    cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update(){
+    if (cursors.left.isDown)
+    {
+      player.setVelocityX(-240); // we want to apply a negative x velocity to go left on the screen
+    }
+    else if (cursors.right.isDown)
+    {
+      player.setVelocityX(240); // we want to apply a positive x velocity to go right on the screen
+    }
+    else
+    {   
+      //if we don't include this then the player will always be going right or left instead of a L/R neutral upwards direction
+      player.setVelocityX(0);
+    }
+
+    if (player.body.touching.down) {
+      player.setVelocityY(-500);
+    }
+
+
+    // We want to the platforms to move downwards and create an effect of progressing upwards
+    // This can be done by moving the camera but can also be done by moving the world around the player
+    if (player.body.y <  gameOptions.height/2) {
+      platforms.children.iterate(updateY, this);
+    }
+  }
+}
+
+let config = {
+  type: Phaser.AUTO,
+  width: gameOptions.width,
+  height: gameOptions.height,
+  backgroundColor: '#4599ff',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: gameOptions.gravity },
+    }
+  },
+  scene: JumpScene,
+};
+
+game = new Phaser.Game(config);
+
+
+// With this function, we move the platforms lower until they're off screen and then we reposition
+// them above the screen to create an endless effect.
+function updateY(platform){
+  let delta = Math.floor(gameOptions.height/2) - player.y;  // we want to keep the player somewhere in the center of the screen so we'll measure the difference from the center y
+
+  if(delta > 0){ 
+    platform.y += delta/30; //the delta may be too large so I'll make it smaller by dividing it by 30
+  }
+
+  if(platform.y > 640){
+    platform.y = -platform.height;
+    platform.x = Math.floor(Math.random() * 400) + 24;
+    platformCount += 1;
+  }
+}
